d280ce43140cc77c8d3dd7a17827caa3dc4ca6c8
Michael Francis Pandy <98488269+mpandy29@users.noreply.github.com>
2022-04-04 19:34:18 -0400

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -2,14 +2,11 @@
 const assertEqual = function(actual, expected) {
   
   if (actual === expected) {
-    return "👍 👍 👍 Assertion Passed: " + actual + ' === ' + expected;
+    return `👍 👍 👍 Assertion Passed: ${actual} === ${expected}`;
 
   } else {
-    return "👎 👎 👎 Assertion Failed: " + actual + " !== " + expected;
+    return `👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`;
   }
 
 };
 
-// TEST CODE
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
