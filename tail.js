4f30431b9a9ca8d3e30b415f9b8dc2a3c0cdc7fa
Michael Francis Pandy <michaelpandy29@gmail.com>
2022-04-05 00:50:55 +0000

    Implement tail

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    return `👍 👍 👍 Assertion Passed: ${actual} === ${expected}`;
+
+  } else {
+    return `👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`;
+  }
+
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words));
+console.log(assertEqual(words.length, 3));
