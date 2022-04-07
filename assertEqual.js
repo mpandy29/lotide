992ef9796919da775c20d198b1b55d42c794d524
Michael Francis Pandy <michaelpandy29@gmail.com>
2022-04-07 00:01:52 +0000

    Updated assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,10 @@
 const assertEqual = function(actual, expected) {
   
   if (actual === expected) {
-    return `👍 👍 👍 Assertion Passed: ${actual} === ${expected}`;
+    console.log  `👍 👍 👍 Assertion Passed: ${actual} === ${expected}`;
 
   } else {
-    return `👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`;
+    console.log `👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`;
   }
 
 };
